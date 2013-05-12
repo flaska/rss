5a0e735a8e372b59b689b5280db216c3b65ca5f0
flaska <jakub.flaska@gmail.com>
2013-05-13 01:10:04 +0200

    'xxx'

--- gen/com/jakubflaska/novinkyrss/R.java
+++ gen/com/jakubflaska/novinkyrss/R.java
@@ -33,29 +33,35 @@ public final class R {
         public static final int iconlauncher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int scrollView1=0x7f090000;
-        public static final int sourceList=0x7f090002;
-        public static final int tableLayout=0x7f090001;
+        public static final int action_settings=0x7f090006;
+        public static final int addnew_address=0x7f090001;
+        public static final int addnew_confirm=0x7f090002;
+        public static final int addnew_name=0x7f090000;
+        public static final int scrollView1=0x7f090003;
+        public static final int sourceList=0x7f090005;
+        public static final int tableLayout=0x7f090004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_rss_list=0x7f030001;
-        public static final int activity_source_list=0x7f030002;
+        public static final int activity_addnew=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_rss_list=0x7f030002;
+        public static final int activity_source_list=0x7f030003;
     }
     public static final class menu {
         public static final int rss_list=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
-        public static final int aktualne=0x7f050006;
+        public static final int addnew_confirm=0x7f050004;
+        public static final int addnew_preset_address=0x7f050003;
+        public static final int addnew_preset_name=0x7f050002;
+        public static final int aktualne=0x7f050008;
         public static final int app_name=0x7f050000;
-        public static final int ct24=0x7f050003;
-        public static final int hello_world=0x7f050002;
-        public static final int idnes=0x7f050004;
-        public static final int ihned=0x7f050008;
-        public static final int lidovky=0x7f050007;
-        public static final int novinky=0x7f050005;
+        public static final int ct24=0x7f050005;
+        public static final int idnes=0x7f050006;
+        public static final int ihned=0x7f05000a;
+        public static final int lidovky=0x7f050009;
+        public static final int novinky=0x7f050007;
     }
     public static final class style {
         /** 
